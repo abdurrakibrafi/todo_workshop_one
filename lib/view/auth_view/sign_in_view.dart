5240e44d627f2b6ffdb6afdf9ea081fce8f211db
rafi <abdurrakibrafi469@gmail.com>
2025-07-23 17:49:11 +0600

    latest code

--- lib/view/auth_view/sign_in_view.dart
+++ lib/view/auth_view/sign_in_view.dart
@@ -7,7 +7,9 @@ class SignInView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("App Bar"),
+      ),
     );
   }
 }
